f655e56b3830c987e8b5c30d30ff51df0f5838a1
Jeff Lombard <jeffl>
2016-07-25 11:25:22 -0700

    added splash screen capability

--- cig.py
+++ cig.py
@@ -1,9 +1,10 @@
 from sys import argv
-from PIL import Image
+from PIL import Image, ImageColor
 from json import load
+from re import match
 import os
 
-# Functions
+# Functions for use in main()
 def check_input_image_size(image):
 
     # Define required image size
@@ -14,8 +15,7 @@ def check_input_image_size(image):
     image_width = image.size[0]
     image_height = image.size[1]
     is_square = (image_width == image_height)
-    is_large_enough = (image_width >= required_width and image_height >=
-                       required_height)
+    is_large_enough = (image_width >= required_width and image_height >= required_height)
     # Return Test
 
     if is_large_enough and is_square:
@@ -25,15 +25,36 @@ def check_input_image_size(image):
     else:
         return (False, "Image is not large enough. Please try again with an image that is at least 192x192.")
 
+def check_color(color):
+    # Use regex to validate hexcode
+    re_result = match('#(\w{6})', color)
+    # Need an if statement, otherwise returning re_result will result in a RE Object
+    if re_result:
+        return True
+    else:
+        return False
+
 def splash(color):
-    pass
+    output_path = 'output/screen/'
+    with open('splash.json') as data:
+        json = load(data)
 
-# Setup
-# Input
-# Transform
-# Output
+    # Make output directory
+    if not os.path.exists(output_path):
+        os.mkdir('output/screen/')
 
-# Main
+    # Build Splash
+    for platform in json:
+        print('Generating '+platform+" splash screens...")
+        if not os.path.exists(output_path+platform):
+            os.mkdir(output_path+platform)
+        for splash in json[platform]:
+            image_width = json[platform][splash][0]
+            image_height = json[platform][splash][1]
+            # Generate image file
+            make = Image.new('RGB',(image_width,image_height),color)
+            make.save(output_path+platform+'/'+splash)
+
 
 def icon(image):
     # Setup/Load
@@ -79,7 +100,99 @@ def main():
 
     icon(image)
 
-    if len(argv) > 2 and argv[2] == 'withSplash':
-        pass
+    icon_xml = """
+    <!-- Copy this code into config.xml -->
+    <platform name="ios">
+        <!-- iOS 8.0+ -->
+        <!-- iPhone 6 Plus  -->
+        <icon src="res/ios/icon-60@3x.png" width="180" height="180" />
+        <!-- iOS 7.0+ -->
+        <!-- iPhone / iPod Touch  -->
+        <icon src="res/ios/icon-60.png" width="60" height="60" />
+        <icon src="res/ios/icon-60@2x.png" width="120" height="120" />
+        <!-- iPad -->
+        <icon src="res/ios/icon-76.png" width="76" height="76" />
+        <icon src="res/ios/icon-76@2x.png" width="152" height="152" />
+        <!-- iOS 6.1 -->
+        <!-- Spotlight Icon -->
+        <icon src="res/ios/icon-40.png" width="40" height="40" />
+        <icon src="res/ios/icon-40@2x.png" width="80" height="80" />
+        <!-- iPhone / iPod Touch -->
+        <icon src="res/ios/icon.png" width="57" height="57" />
+        <icon src="res/ios/icon@2x.png" width="114" height="114" />
+        <!-- iPad -->
+        <icon src="res/ios/icon-72.png" width="72" height="72" />
+        <icon src="res/ios/icon-72@2x.png" width="144" height="144" />
+        <!-- iPhone Spotlight and Settings Icon -->
+        <icon src="res/ios/icon-small.png" width="29" height="29" />
+        <icon src="res/ios/icon-small@2x.png" width="58" height="58" />
+        <!-- iPad Spotlight and Settings Icon -->
+        <icon src="res/ios/icon-50.png" width="50" height="50" />
+        <icon src="res/ios/icon-50@2x.png" width="100" height="100" />
+    </platform>
+    <platform name="android">
+        <!--
+            ldpi    : 36x36 px
+            mdpi    : 48x48 px
+            hdpi    : 72x72 px
+            xhdpi   : 96x96 px
+            xxhdpi  : 144x144 px
+            xxxhdpi : 192x192 px
+        -->
+        <icon src="res/android/ldpi.png" density="ldpi" />
+        <icon src="res/android/mdpi.png" density="mdpi" />
+        <icon src="res/android/hdpi.png" density="hdpi" />
+        <icon src="res/android/xhdpi.png" density="xhdpi" />
+        <icon src="res/android/xxhdpi.png" density="xxhdpi" />
+        <icon src="res/android/xxxhdpi.png" density="xxxhdpi" />
+    </platform>
+    """
+
+    print("Don't forget to copy the html in _text_output.txt into config.xml")
+
+    # Make Text file
+
+    with open('output/_text_output.txt', 'w+') as data:
+        data.write(icon_xml)
+
+    # Checks for Splash Screen
+    if len(argv) > 2 and argv[2] == '-ws':
+        color = input("Enter the hex color for the splash screen (including the # sign):")
+        while check_color(color) == False:
+            if color == 'q':
+                return False
+            color = input("Please enter a valid hex value preceded by a # or 'q' to quit:")
+
+        splash(color)
+        splash_xml = """
+
+    <platform name="android">
+        <!-- you can use any density that exists in the Android project -->
+        <splash src="res/screen/android/splash-land-hdpi.png" density="land-hdpi"/>
+        <splash src="res/screen/android/splash-land-ldpi.png" density="land-ldpi"/>
+        <splash src="res/screen/android/splash-land-mdpi.png" density="land-mdpi"/>
+        <splash src="res/screen/android/splash-land-xhdpi.png" density="land-xhdpi"/>
+
+        <splash src="res/screen/android/splash-port-hdpi.png" density="port-hdpi"/>
+        <splash src="res/screen/android/splash-port-ldpi.png" density="port-ldpi"/>
+        <splash src="res/screen/android/splash-port-mdpi.png" density="port-mdpi"/>
+        <splash src="res/screen/android/splash-port-xhdpi.png" density="port-xhdpi"/>
+    </platform>
+
+    <platform name="ios">
+        <!-- images are determined by width and height. The following are supported -->
+        <splash src="res/screen/ios/Default~iphone.png" width="320" height="480"/>
+        <splash src="res/screen/ios/Default@2x~iphone.png" width="640" height="960"/>
+        <splash src="res/screen/ios/Default-Portrait~ipad.png" width="768" height="1024"/>
+        <splash src="res/screen/ios/Default-Portrait@2x~ipad.png" width="1536" height="2048"/>
+        <splash src="res/screen/ios/Default-Landscape~ipad.png" width="1024" height="768"/>
+        <splash src="res/screen/ios/Default-Landscape@2x~ipad.png" width="2048" height="1536"/>
+        <splash src="res/screen/ios/Default-568h@2x~iphone.png" width="640" height="1136"/>
+        <splash src="res/screen/ios/Default-667h.png" width="750" height="1334"/>
+        <splash src="res/screen/ios/Default-736h.png" width="1242" height="2208"/>
+        <splash src="res/screen/ios/Default-Landscape-736h.png" width="2208" height="1242"/>
+    </platform>"""
+        with open('output/_text_output.txt', 'a') as data:
+            data.write(splash_xml)
 
 main()
